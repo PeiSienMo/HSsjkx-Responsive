2c49c3e39619a745cd8e8e4bf2ff6c2136c64aa1
PeiSienMo <40021581+PeiSienMo@users.noreply.github.com>
2019-05-17 18:00:52 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    //定义变量获取屏幕视口宽度
+    var windowWidth = parseInt($(window).width());
+    if(windowWidth > 992){
+        $(".dropdown").hover(function () {
+            $(this).addClass("open");
+            var a = $(this).find("a").first();
+            $(a).attr("aria-expanded","true");
+        },function () {
+            $(this).removeClass("open");
+            var a = $(this).find("a").first();
+            $(a).attr("aria-expanded","false");
+        });
+    }
+    $(function() {
+        //轮播图自动播放
+        $('#myCarousel').carousel({
+            interval: 3000,//自动播放4s
+
+        });
+    });
+
+});
